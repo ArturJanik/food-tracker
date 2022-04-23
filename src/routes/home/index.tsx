f4bd6c5e4042823306401dbf836261a9d4bdfb06
ArturJanik <sidsky@o2.pl>
2022-04-23 13:40:14 +0200

    COMMIT-12 | Add pin on enter

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,25 +1,55 @@
 import { useStore } from '@nanostores/preact';
 import { FunctionalComponent, h } from 'preact';
-import { useEffect } from 'preact/hooks';
+import { useEffect, useState } from 'preact/hooks';
 import FoodList from '../../components/FoodList';
 import Summary from '../../components/Summary';
+import Button from '../../components/UI/Button';
+import Input from '../../components/UI/Input';
 import Spinner from '../../components/UI/Spinner';
 import { foodnotesLoading, getFoodnotes } from '../../store/foodnotes';
 import { foods, foodsLoading, getFoods } from '../../store/foods';
 import style from './style.css';
 
+const REQUIRED_PIN = '0700';
+
 const Home: FunctionalComponent = () => {
     const data = useStore(foods);
     const loading = useStore(foodsLoading);
     const notesLoading = useStore(foodnotesLoading);
+    const [authorized, setAuthorized] = useState(false);
+    const [pin, setPin] = useState('');
 
     useEffect(() => {
-        getFoods();
-    }, []);
+        if (authorized) {
+            getFoods();
+        }
+    }, [authorized]);
 
     useEffect(() => {
         getFoodnotes();
     }, [data]);
+
+    const validate = () => {
+        if (pin === REQUIRED_PIN) {
+            setAuthorized(true);
+        }
+    }
+
+    if (!authorized) {
+        return (
+            <div class={style.home}>
+                <Input
+                    type='text'
+                    id='pin'
+                    name='pin' 
+                    onInput={(e) => setPin(e.currentTarget.value)}
+                    placeholder='Enter pin'
+                    value={pin}
+                />
+                <Button classes={style.btn} onClick={validate}>Submit</Button>
+            </div>
+        );
+    }
 
     return (
         <div class={style.home}>
